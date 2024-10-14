7ea1b962d08f3553a776d88ab96260f72582477e
Swagatam Biswas <63900743+BiswasSwagatam@users.noreply.github.com>
2024-10-14 13:08:10 +0530

    Update Projects.jsx

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,6 +2,7 @@
 import './ProjectsStyles.Module.css'
 import viberr from '../../assets/viberr.png'
 import moody from '../../assets/favicon.png'
+import xlogo from '../../assets/X_logo.jpg'
 import ProjectCard from '../../common/ProjectCard'
 
 function Projects() {
@@ -11,6 +12,7 @@ function Projects() {
         <div className='projectscontainer'>
             <ProjectCard link="https://github.com/BiswasSwagatam/MERN---Ecommerce-Store" src={viberr} h3="E-Commerce Store" p="React JS, Node JS, Express JS, MongoDB, Redis"/>
             <ProjectCard link="https://github.com/BiswasSwagatam/Moody---Mood-tracker-app" src={moody} h3="Daily Mood Tracker" p="React JS, NEXT JS, Tailwind CSS, Firebase"/>
+            <ProjectCard link="https://github.com/BiswasSwagatam/X-clone" src={xlogo} h3="X - clone" p="React JS, Node JS, Express JS, Tailwind CSS, MongoDB, Cloudinary"/>
         </div>
     </section>
   )
